813ac991f750b4674f8b68f5fc838486dd6f0ab0
opw226 <opw226@lehigh.edu>
2025-10-02 12:31:51 -0400

    Update tb_fir.cpp

--- tb_fir.cpp
+++ tb_fir.cpp
@@ -7,20 +7,17 @@ typedef ap_int<32> acc_t;
 const int N_TAPS = 16;
 const int N_SAMPLES = 256;
 
-// Prototype of the HLS FIR function
 void fir(const data_t x[N_SAMPLES], data_t y[N_SAMPLES]);
 
 int main() {
     data_t x[N_SAMPLES], y[N_SAMPLES];
 
-    // Impulse input = 1 (good for showing FIR coefficients in the output)
     for (int i = 0; i < N_SAMPLES; i++) {
         x[i] = (i == 0) ? 1 : 0;
     }
 
     fir(x, y);
 
-    // Print first 20 outputs for inspection
     printf("Impulse response (first 20 samples):\n");
     for (int i = 0; i < 20; i++) {
         printf("y[%d] = %d\n", i, (int)y[i]);
